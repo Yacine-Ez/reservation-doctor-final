e37c7ee95ff3cfda1b09c9491b734c10adf69c8b
Yacine <yacineae127@gmail.com>
2026-03-19 06:22:31 +0000

    update server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: "https://reservation-doctor-final-1.onrender.com/api"|| "/api",
 });
 
 export const getDoctors = () => API.get("/doctors");
