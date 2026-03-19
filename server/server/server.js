dfe0ad253c6509d2054c1630cba6a684820185bf
Yacine <yacineae127@gmail.com>
2026-03-19 03:42:35 +0000

    fix: add root route

--- server/server/server.js
+++ server/server/server.js
@@ -5,7 +5,9 @@ const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
-
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
